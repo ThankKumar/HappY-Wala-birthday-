8d1acb2b0375561428f69b8d1c28bd2952033676
Ram Uday Kumar <56128152+ThankKumar@users.noreply.github.com>
2022-11-30 07:51:06 +0530

    nan

---
+++ config.js
@@ -0,0 +1,29 @@
+
+var config = {
+
+    texts: [
+        "Happy Birthday Manoj sir", 
+        "I wish you a happier birthday than anyone else has wished you.", 
+        "हमें समय का महत्त्व बताने के लिए और सर्वश्रेष्ठ होने के लिए प्रेरित करने के लिए आपको दिल से शुक्रिया।",
+        "मैं अपनी जिंदगी मे आपकी तरह सज्जन और एक सक्षम बॉस की जैसा काम करना चाहता हूं। जन्मदिन की शुभकामना सर!",
+        "हम आपको आपके जन्मदिन पर बधाई देता हूं और भगवान से यहीं दुआ है की आपको काम में सफलता और खुशी मिले।",
+        "हैप्पी बर्थडे मेरे प्यारे गुरूजी..!",
+        "Thank You sir.",
+    ],
+    
+    imgs: {
+        "心爱的小可爱": "./imgs/WhatsApp Image 2022-11-29 at 18.34.06.jpeg",
+        
+    },
+    
+    desc: {
+        turn_on: "Next",
+        play: "Play",
+        bannar_coming: "Next",
+        balloons_flying: "Happy",
+        cake_fadein: "Cake",
+        light_candle: "Light",
+        wish_message: "I wish you Happy Birthday",
+        story: "A MESSAGE FOR YOU",
+    }
+};
